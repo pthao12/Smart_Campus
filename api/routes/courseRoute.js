97f520b6ad044fde9685e9d6cc4195e2278aaaf9
Do-Trang <thutrang25062k4@gmail.com>
2023-11-27 12:50:07 +0700

    feat: update course UI

--- api/routes/courseRoute.js
+++ api/routes/courseRoute.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { getAllCourse } from "../controllers/courseController.js";
+import { getAllCourse, addNewCourse, deleteCourse, updateCourse } from "../controllers/courseController.js";
 
 const courseRoute = Router();
 courseRoute.get("/", getAllCourse);
-
+courseRoute.get("/addCourse", addNewCourse);
+courseRoute.get("/deleteCourse/:id", deleteCourse);
+courseRoute.get("/updateCourse/:id", updateCourse);
 
 export default courseRoute;
