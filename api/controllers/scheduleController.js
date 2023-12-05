307f6efb46ca7d17caec80a3c8d581670dbfeb0b
Vương Phương Thảo <125387948+pthao12@users.noreply.github.com>
2023-12-05 23:12:47 +0700

    add link document

--- api/controllers/scheduleController.js
+++ api/controllers/scheduleController.js
@@ -1,7 +1,9 @@
 import db from '../connect/connect.js';
 
 export const getSchedule = (req, res) => {
-    let sql = "select concat(courseID, ' ', sectionID) as class ,  semester, year, day, timeStart, timeEnd from sections;";
+    let sql = "select c.courseName, concat(s.courseID, ' ', s.sectionID) as class ,  s.semester, s.year, s.day, s.timeStart, s.timeEnd "
+            + "from sections s "
+            + "join courses c using (courseID);";
     db.query(sql, function(err, data){
         console.log(data);
         if(err) throw err;
